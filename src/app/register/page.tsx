9c757a8e57f871cbd95cc6ed159032a93e7f59a3
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2025-03-17 15:09:05 -0400

    header component for authentication pages

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,23 +1,12 @@
 "use client";
 import React from 'react';
-import {useRouter} from "next/navigation";
 import Footer from '@/components/Footer';
+import AuthHeader from '@/components/AuthHeader';
 
 const Page = () => {
-     const router = useRouter();
-    
-    const handleLogoClick = () => {
-        router.push('/');
-    };
-
     return (
         <div className="page-container">
-            <header className="header flex justify-between items-center bg-blue-800 text-white p-4">
-                <div className="items-center flex gap-2">
-                    <img src="logo_no_back.png" alt="FlyNext Logo" className="h-8" onClick={handleLogoClick}/>
-                    <h1 className="text-2xl" onClick={handleLogoClick}>FlyNext</h1>
-                </div>
-            </header>
+            <AuthHeader />
             <main>
                 <div className="loginBox">
                     {/* login form box goes here */}
