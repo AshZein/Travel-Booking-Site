661adb3fe4f790e8089233b6be625c05640b8f76
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2025-03-17 15:13:56 -0400

    hotel location suggestion dropdowns

--- src/components/homePage/hotel.tsx
+++ src/components/homePage/hotel.tsx
@@ -2,11 +2,29 @@ import React, { useState, useEffect } from 'react';
 import { useRouter } from "next/navigation";
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import { searchSuggestAirports, searchSuggestCities } from '@/utils/cleanSearch';
+import { searchSuggestCities } from '@/utils/cleanSearch';
 
 const Hotel: React.FC = () => {
+    const hotelRouter = useRouter();
+
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
+
+    const [destinationLocation, setDestinationLocation] = useState('');
+    const [destinationSuggestions, setDestinationSuggestions] = useState<string[]>([]);
+
+    const handleDestinationLocationChange = async (value: string) => {
+        setDestinationLocation(value);
+
+        const suggestions = await searchSuggestCities(value);
+        
+        setDestinationSuggestions(suggestions);
+    };
+
+    const handleDestinationSuggestionClick = (suggestion: string) => {
+        setDestinationLocation(suggestion);
+        setDestinationSuggestions([]);
+    };
 
     return (
         <div className="flex items-center gap-4">
@@ -17,7 +35,22 @@ const Hotel: React.FC = () => {
                     id="hotel-location" 
                     className="text-black p-2 rounded" 
                     placeholder="Enter location"
+                    onChange={(e) => handleDestinationLocationChange(e.target.value)}
+                    value={destinationLocation}
                 />
+                {destinationSuggestions.length > 0 && (
+                        <ul className="suggestions-list">
+                            {destinationSuggestions.map((suggestion, index) => (
+                                <li 
+                                    key={index} 
+                                    className="suggestion-item"
+                                    onClick={() => handleDestinationSuggestionClick(suggestion)}
+                                >
+                                    {suggestion}
+                                </li>
+                            ))}
+                        </ul>
+                    )}
             </div>
             <div className="search-box">
                 <label htmlFor="start-date" className="text-white">Start Date:</label>
