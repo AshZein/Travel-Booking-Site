5ff543e0160decf4880463ff9728f96362d83f6b
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2025-03-16 21:25:11 -0400

    function for cleaning up output and fetching suggestions

---
+++ src/utils/cleanSearch.ts
@@ -0,0 +1,25 @@
+async function searchSuggestCities(search: string) {
+    try {
+        const response = await fetch(`http://localhost:3000/api/airport?query=${search}`);
+        const data = await response.json();
+        const cities = data.data.cities.map((city: { city: string; country: string }) => `${city.city}, ${city.country}`);
+        return cities;
+    } catch (error) {
+        console.error('Error fetching suggestions:', error);
+        return [];
+    }
+}
+
+async function searchSuggestAirports(search: string) {
+    try {
+        const response = await fetch(`http://localhost:3000/api/airport?query=${search}`);
+        const data = await response.json();
+        const airports = data.data.airports.map((airport: { name: string; code: string; country: string }) => `${airport.name} (${airport.code}), ${airport.country}`);
+        return airports;
+    } catch (error) {
+        console.error('Error fetching suggestions:', error);
+        return [];
+    }
+}
+
+export { searchSuggestAirports, searchSuggestCities };
