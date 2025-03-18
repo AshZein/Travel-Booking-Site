179edfcc9d2a70a04faedb68f74a8316902f7c49
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2025-03-18 17:44:51 -0400

    flight search component files'

---
+++ src/components/flightSearch/FlightResults.tsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import 'react-datepicker/dist/react-datepicker.css';
+
+const FlightResults: React.FC = () => {
+    return (
+        
+    );
+}
+
+export default FlightResults;
