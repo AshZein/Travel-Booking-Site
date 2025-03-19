abbd2a68d641e8f0f3bf19cc4ab283645f40c9bc
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2025-03-19 12:27:40 -0400

    flight interface added

--- src/components/itinerary/FlightBooking.tsx
+++ src/components/itinerary/FlightBooking.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
 
+interface Flight {
+    id: string;
+    flightNumber: string;
+    departureTime: string;
+    arrivalTime: string;
+    origin: {
+        code: string;
+        name: string;
+        city: string;
+        country: string;
+    };
+    destination: {
+        code: string;
+        name: string;
+        city: string;
+        country: string;
+    };
+    duration: number;
+    price: number;
+    currency: string;
+    availableSeats: number;
+    status: string;
+    airline: {
+        code: string;
+        name: string;
+    };
+}
+
+interface FlightBookingProps {
+    flights: Flight[];
+    outBoundFlight: boolean;
+}
 const FlightBooking: React.FC = () => {
     return (
         <div>
-            
+            <p>TEST ONE</p>
         </div>
     );
 }
