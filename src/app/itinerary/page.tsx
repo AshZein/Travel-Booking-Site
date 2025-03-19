d3890817afeb3f6360cd4ec5dd94cf038cf71196
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2025-03-19 12:04:19 -0400

    itinerary redirect, and page setup

---
+++ src/app/itinerary/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React, { useState } from 'react';
+import HomeHeader from '@/components/HomeHeader';
+import Footer from '@/components/Footer';
+
+const Page = () => {
+    return (
+        <div>
+            <HomeHeader />
+
+            
+
+            <Footer />
+        </div>
+    );
+}
+
+export default Page;
