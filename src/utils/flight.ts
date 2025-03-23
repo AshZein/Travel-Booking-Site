42fb86da05642d8dd0f210ea61c15b96b035a2e3
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2025-03-23 19:58:21 -0400

    checkout utils file, moved bookflights to here

--- src/utils/flight.ts
+++ src/utils/flight.ts
@@ -33,36 +33,4 @@ function totalFlightCost(flights: Flight[]) {
     return flights.reduce((total, flight) => total + flight.price, 0);
 }
 
-async function bookFlights(firstName: string, lastName: string, email: string, passportNumber: string, flights: Flight[]) {
-    console.log('Booking flights:', flights);
-
-    const bookingData = {
-        firstName,
-        lastName,
-        email,
-        passportNumber,
-        flightIds: flights.map(flight => flight.id),
-    };
-
-    try {
-        const response = await fetch('http://localhost:3000/api/flight/booking', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(bookingData),
-        });
-
-        if (!response.ok) {
-            throw new Error(`Error: ${response.statusText}`);
-        }
-
-        const result = await response.json();
-        console.log('Booking successful:', result);
-        return result;
-    } catch (error) {
-        console.error('Booking failed:', error);
-    }
-}
-
 export { totalFlightCost, dateSplitter };
