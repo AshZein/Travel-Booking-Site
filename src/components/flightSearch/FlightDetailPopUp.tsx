fa1d37ef552a46333d7405da1e10388d31951e68
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2025-03-23 17:56:42 -0400

    commented out flight interface removed

--- src/components/flightSearch/FlightDetailPopUp.tsx
+++ src/components/flightSearch/FlightDetailPopUp.tsx
@@ -37,34 +37,6 @@ function totalFlightCost(flights: Flight[]) {
     return flights.reduce((total, flight) => total + flight.price, 0);
 }
 
-// interface Flight {
-//     id: string;
-//     flightNumber: string;
-//     departureTime: string;
-//     arrivalTime: string;
-//     origin: {
-//         code: string;
-//         name: string;
-//         city: string;
-//         country: string;
-//     };
-//     destination: {
-//         code: string;
-//         name: string;
-//         city: string;
-//         country: string;
-//     };
-//     duration: number;
-//     price: number;
-//     currency: string;
-//     availableSeats: number;
-//     status: string;
-//     airline: {
-//         code: string;
-//         name: string;
-//     };
-// }
-
 interface FlightCardProps {
     legs: number;
     flights: Flight[];
