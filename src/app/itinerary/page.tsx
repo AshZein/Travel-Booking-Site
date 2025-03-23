77f3b834e5d5a0c87add917c09e170b6ee50eb61
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2025-03-23 19:44:10 -0400

    itinerary

--- src/app/itinerary/page.tsx
+++ src/app/itinerary/page.tsx
@@ -6,9 +6,22 @@ import FlightBooking from '@/components/itinerary/FlightBooking';
 import HotelBooking from '@/components/itinerary/HotelBooking';
 import { useItinerary } from '@/context/ItineraryContext';
 import withItineraryProvider from '@/HOC/withItineraryProvider';
+import { Flight } from '@/types/flight';
+import FlightDetailPopUp from '@/components/flightSearch/FlightDetailPopUp';
 
 const Page = () => {
     const { state, dispatch } = useItinerary();
+    const [showPopup, setShowPopup] = useState(false); // State for showing popup
+    const [selectedFlight, setSelectedFlight] = useState<Flight[] | null>(null); // State for selected flight
+    
+    const handleFlightClick = (flights: Flight[]) => {
+        setSelectedFlight(flights);
+        setShowPopup(true);
+    };
+
+    const closePopup = () => {
+        setShowPopup(false);
+    };
 
     return (
         <div>
@@ -21,9 +34,18 @@ const Page = () => {
                     flights={state.selectedOutboundFlights}
                     legs={state.selectedOutboundFlights.length}
                     outBoundFlight={true}
-                    onClick={() => {}}
+                    onClick={() => handleFlightClick(state.selectedOutboundFlights)}
                     />
                 )}
+
+            {showPopup && selectedFlight && (
+                <div className="popup">
+                    <div className="popup-content">
+                        <button onClick={closePopup} className="tripType-button p-2">✕</button>
+                        <FlightDetailPopUp legs={selectedFlight.length} flights={selectedFlight} />
+                    </div>
+                </div>
+            )}
             </main>
 
             <Footer />
