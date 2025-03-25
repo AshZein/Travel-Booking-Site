6868f45028eaaf12a7defaae00bb0bea3f560e25
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2025-03-25 15:02:29 -0400

    notification drop down component file made

---
+++ src/components/NotificationDrop.tsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import { useRouter } from "next/navigation";
+
+const NotificationDrop: React.FC = () => {
+    return(
+       <div></div> 
+    );
+}
+
+export default NotificationDrop;
