92631cb94047c1349fd4aaa7e68755d220528c20
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2025-03-25 15:52:10 -0400

    date format

--- src/components/Notification/dropdown/NotificationCard.tsx
+++ src/components/Notification/dropdown/NotificationCard.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import { useRouter } from "next/navigation";
+import React from 'react';
 
 interface NotificationCardProps {
     message: string;
@@ -7,12 +6,23 @@ interface NotificationCardProps {
     read: boolean;
 }
 
-const NotificationCard: React.FC<NotificationCardProps> = ({message, date, read}) => {
+const NotificationCard: React.FC<NotificationCardProps> = ({ message, date, read }) => {
+    // Format the date
+    const formatDate = (isoDate: string) => {
+        const dateObj = new Date(isoDate);
+        const year = dateObj.getFullYear();
+        const month = String(dateObj.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+        const day = String(dateObj.getDate()).padStart(2, '0');
+        const hours = String(dateObj.getHours()).padStart(2, '0');
+        const minutes = String(dateObj.getMinutes()).padStart(2, '0');
+        return `${year}-${month}-${day} ${hours}:${minutes}`;
+    };
+
     return (
         <div 
             className={`p-4 border rounded-md shadow-md ${read ? 'text-gray-500' : 'text-black'}`} 
         >
-            <div className="text-sm text-left text-gray-400">{date}</div>
+            <div className="text-sm text-left text-gray-400">{formatDate(date)}</div>
             <div className="text-lg font-semibold">{message}</div>
         </div>
     );
