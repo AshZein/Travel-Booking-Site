0b9817c9aec1847479b4291df22c13e6d4fa3a3e
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2025-03-25 15:44:02 -0400

    notification page

---
+++ src/app/profile/notifications/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React, { useState } from 'react';
+import withItineraryProvider from '@/HOC/withItineraryProvider';
+import HomeHeader from '@/components/HomeHeader';
+import Footer from '@/components/Footer';
+
+const Page = () => {
+    return (
+        <div></div>
+    );  
+}
+
+export default withItineraryProvider(Page);
