a1a892082a80117ae5414ca6bf370eb976302672
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2025-03-26 12:04:20 -0400

    create an admin user

--- src/utils/runPopulate.js
+++ src/utils/runPopulate.js
@@ -1,4 +1,25 @@
 import { fillAirport, fillCity } from './populateAirportCity.js';
+import { prisma } from "@/utils/db";
+
+async function createAdmingUser() {
+    prisma.User.delete({
+        where: {
+            userId: 1
+        }});
+
+    const admin = await prisma.user.create({
+        data: {
+            userId: 1,
+            password: "123",
+            firstName: "hotel",
+            lastName: "admin",
+            email: "hoteladmin@admin.com",
+            phoneNumber: "1234567890",
+        }
+    });
+
+    console.log('Created admin user:', admin);
+}
 
 async function run() {
     console.log('Starting to populate airports and cities...');
