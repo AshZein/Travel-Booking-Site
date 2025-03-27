3ed160477f15a05be72ec466d2e74e54579294c5
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2025-03-26 21:38:34 -0400

    checkout flight card

--- src/components/checkout/checkoutFlightCard.tsx
+++ src/components/checkout/checkoutFlightCard.tsx
@@ -4,6 +4,8 @@ import { useItinerary } from '@/context/ItineraryContext';
 import { dateSplitter, totalFlightCost } from '@/utils/flight';
 import { Flight } from '@/types/flight';
 
+const dateCache: { [key: string]: { [key: string]: string } } = {};
+
 interface FlightCardProps {
     legs: number;
     flights: Flight[];
@@ -13,9 +15,46 @@ interface FlightCardProps {
 }
 
 const CheckoutFlightCard: React.FC<FlightCardProps> = ({ legs, flights, onClick, onAddToItinerary, type }) => {
+    const { state, dispatch } = useItinerary();
+    const firstFlightId = flights[0].id;
+    const lastFlightId = flights[flights.length - 1].id;
+    
+    const renderFlight = (outBoundFlight: Flight, inBoundFlight: Flight) => {
+        const departurePieces = dateSplitter(outBoundFlight.departureTime, dateCache);
+                const arrivalPieces = dateSplitter(inBoundFlight.arrivalTime, dateCache);
+                
+                return (
+                    <div key={outBoundFlight.id} className="flight-details flex items-center gap-20" onClick={onClick}>
+                        <div className="flex flex-col items-center gap-1">
+                            <p>{departurePieces.hour}:{departurePieces.minute}</p>
+                            <p>{outBoundFlight.origin.code} - {departurePieces.day} {departurePieces.month}</p>
+                        </div>
+                        <div>
+                            {legs - 1 > 1 ? (
+                                <>{legs - 1} Stops</>
+                            ) : legs - 1 === 1 ? (
+                                <>1 Stop  </>
+                            ) : (
+                                <>DIRECT</>
+                            )}
+                        </div>
+                        <div className="flex flex-col items-center gap-1">
+                            <p>{arrivalPieces.hour}:{arrivalPieces.minute}</p>
+                            <p>{inBoundFlight.destination.code} - {arrivalPieces.day} {arrivalPieces.month}</p>
+                        </div>
+                        
+                        <div className="vertical-line border-l-2 border-black h-full pl-4">
+                            <p><strong>{outBoundFlight.currency} {totalFlightCost(flights)}</strong></p>
+                        </div>
+                    </div>
+                );
+    };
+
     return(
         <div>
-            <h1>Checkout Flight Card</h1>
+            <>
+            {renderFlight(flights[0], flights[flights.length - 1])}
+            </>
         </div>
     );
 }
