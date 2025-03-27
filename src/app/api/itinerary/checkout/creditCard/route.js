7782a892d27e74b551de0ad9ca0a2392389de5ba
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2025-03-27 15:12:33 -0400

    credit card verification endpoint implemented

---
+++ src/app/api/itinerary/checkout/creditCard/route.js
@@ -0,0 +1,47 @@
+import { prisma } from "@/utils/db";
+import { NextResponse } from "next/server";
+import { verifyToken } from "@/utils/auth";
+
+// Luhn algorithm to validate credit card number
+function isValidCreditCardNumber(number) {
+    const reversedDigits = number.split('').reverse().map(digit => parseInt(digit, 10));
+    const sum = reversedDigits.reduce((acc, digit, idx) => {
+        if (idx % 2 === 1) {
+            const doubled = digit * 2;
+            return acc + (doubled > 9 ? doubled - 9 : doubled);
+        }
+        return acc + digit;
+    }, 0);
+    return sum % 10 === 0;
+}
+
+export async function POST(request){
+    // verify user token
+    const user = verifyToken(request);
+    if (!user) {
+        return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    const { creditCardNumber, creditCardExpiry, creditCardCVC } = await request.json();
+    if (!creditCardNumber || !creditCardExpiry || !creditCardCVC){
+        return NextResponse.json({ message: "creditCardNumber, creditCardExpiry, and creditCardCVC are required parameters" }, { status: 400 });
+    }
+    if (typeof creditCardNumber !== 'string' || typeof creditCardExpiry !== 'string' || typeof creditCardCVC !== 'string'){
+        return NextResponse.json({ message: "creditCardNumber, creditCardExpiry, and creditCardCVC must be strings" }, { status: 400 });
+    }
+
+    // check if credit card expiry is greater than now
+    const currentDate = new Date();
+    const [month, year] = creditCardExpiry.split('/').map(Number);
+    const expiryDate = new Date(year, month);
+
+    if (expiryDate <= currentDate) {
+        return NextResponse.json({ message: "Credit card has expired" }, { status: 400 });
+    }
+
+    // check if credit card number is valid
+    if (!isValidCreditCardNumber(creditCardNumber)){
+        return NextResponse.json({ message: "Invalid credit card number" }, { status: 400 });
+    }
+    return NextResponse.json({ message: "Credit card is valid" }, { status: 200 });
+}
