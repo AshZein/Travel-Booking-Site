159c4e125583346202693d3c7257e405c70eda3e
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2025-03-26 23:14:08 -0400

    save to checkout context

--- src/components/checkout/Credentials.tsx
+++ src/components/checkout/Credentials.tsx
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useCheckout } from '@/context/CheckoutContext'; // Import the CheckoutContext
+import { FlightCred } from '@/types/FlightCred'; // Import the FlightCred type
 
 const FlightCredentials: React.FC = () => {
+    const { dispatch } = useCheckout(); // Access the CheckoutContext dispatch function
+    const [flightCred, setFlightCred] = useState<FlightCred>({
+        firstName: '',
+        middleName: '',
+        lastName: '',
+        passportNumber: '',
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFlightCred((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        // Dispatch the flight credentials to the CheckoutContext
+        dispatch({ type: 'SET_FLIGHT_CREDENTIALS', payload: flightCred });
+        alert('Flight credentials saved to checkout!');
+    };
+
     return (
         <div>
             <h1 className="text-black bg-white">Booking Information</h1>
-            <form className='flex flex-col gap-6 p-6 shadow-md bg-white'>
+            <form
+                className="flex flex-col gap-6 p-6 shadow-md bg-white"
+                onSubmit={handleSubmit}
+            >
                 <div>
                     <label htmlFor="firstName" className="block mb-1 font-medium text-black">First Name:</label>
                     <input
                         type="text"
                         id="firstName"
                         name="firstName"
+                        value={flightCred.firstName}
+                        onChange={handleChange}
                         className="w-full p-2 border border-black rounded text-black"
                     />
                 </div>
@@ -20,6 +50,8 @@ const FlightCredentials: React.FC = () => {
                         type="text"
                         id="middleName"
                         name="middleName"
+                        value={flightCred.middleName}
+                        onChange={handleChange}
                         className="w-full p-2 border border-black rounded text-black"
                     />
                 </div>
@@ -29,6 +61,8 @@ const FlightCredentials: React.FC = () => {
                         type="text"
                         id="lastName"
                         name="lastName"
+                        value={flightCred.lastName}
+                        onChange={handleChange}
                         className="w-full p-2 border border-black rounded text-black"
                     />
                 </div>
@@ -38,9 +72,12 @@ const FlightCredentials: React.FC = () => {
                         type="text"
                         id="passportNumber"
                         name="passportNumber"
+                        value={flightCred.passportNumber}
+                        onChange={handleChange}
                         className="w-full p-2 border border-black rounded text-black"
                     />
                 </div>
+                <button type="submit" className="p-2 bg-blue-500 text-white rounded">Submit</button>
             </form>
         </div>
     );
--- src/components/checkout/CreditCardInfo.tsx
+++ src/components/checkout/CreditCardInfo.tsx
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useCheckout } from '@/context/CheckoutContext'; // Import the CheckoutContext
+import { CreditCard } from '@/types/CreditCard'; // Import the CreditCard type
 
 const CreditCardInfo: React.FC = () => {
+    const { dispatch } = useCheckout(); // Access the CheckoutContext dispatch function
+    const [creditCard, setCreditCard] = useState<CreditCard>({
+        cardName: '',
+        cardNumber: '',
+        cvc: '',
+        expiryMonth: '',
+        expiryYear: '',
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const { name, value } = e.target;
+        setCreditCard((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        // Dispatch the credit card info to the CheckoutContext
+        dispatch({ type: 'SET_CREDIT_CARD_INFO', payload: creditCard });
+        alert('Credit card information saved to checkout!');
+    };
+
     return (
         <div>
             <h1 className="text-black bg-white">Credit Card Information</h1>
-            <form className='flex flex-col gap-6 p-6 shadow-md bg-white'>
+            <form
+                className="flex flex-col gap-6 p-6 shadow-md bg-white"
+                onSubmit={handleSubmit}
+            >
                 <div>
                     <label htmlFor="cardName" className="block mb-1 font-medium text-black">Name on Card:</label>
                     <input
                         type="text"
                         id="cardName"
                         name="cardName"
+                        value={creditCard.cardName}
+                        onChange={handleChange}
                         className="w-full p-2 border border-black rounded text-black"
                     />
                 </div>
@@ -20,6 +51,8 @@ const CreditCardInfo: React.FC = () => {
                         type="text"
                         id="cardNumber"
                         name="cardNumber"
+                        value={creditCard.cardNumber}
+                        onChange={handleChange}
                         className="w-full p-2 border border-black rounded text-black"
                     />
                 </div>
@@ -29,6 +62,8 @@ const CreditCardInfo: React.FC = () => {
                         type="text"
                         id="cvc"
                         name="cvc"
+                        value={creditCard.cvc}
+                        onChange={handleChange}
                         className="w-full p-2 border border-black rounded text-black"
                     />
                 </div>
@@ -38,6 +73,8 @@ const CreditCardInfo: React.FC = () => {
                         <select
                             id="expiryMonth"
                             name="expiryMonth"
+                            value={creditCard.expiryMonth}
+                            onChange={handleChange}
                             className="w-1/2 p-2 border border-black rounded text-black"
                         >
                             <option value="">Month</option>
@@ -50,6 +87,8 @@ const CreditCardInfo: React.FC = () => {
                         <select
                             id="expiryYear"
                             name="expiryYear"
+                            value={creditCard.expiryYear}
+                            onChange={handleChange}
                             className="w-1/2 p-2 border border-black rounded text-black"
                         >
                             <option value="">Year</option>
@@ -61,6 +100,12 @@ const CreditCardInfo: React.FC = () => {
                         </select>
                     </div>
                 </div>
+                <button
+                    type="submit"
+                    className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+                >
+                    Save Credit Card Info
+                </button>
             </form>
         </div>
     );
