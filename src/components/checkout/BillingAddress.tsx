c27029339df27b362c2d9c68c1636a508d74e7ec
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2025-03-26 23:10:25 -0400

    save billing address to checkoug context

--- src/components/checkout/BillingAddress.tsx
+++ src/components/checkout/BillingAddress.tsx
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useCheckout } from '@/context/CheckoutContext'; // Import the CheckoutContext
+import { Billing } from '@/types/Billing'; // Import the Billing type
 
 const BillingAddress: React.FC = () => {
+    const { dispatch } = useCheckout(); // Access the CheckoutContext dispatch function
+    const [billingAddress, setBillingAddress] = useState<Billing>({
+        firstName: '',
+        lastName: '',
+        streetAddress: '',
+        city: '',
+        province: '',
+        postalCode: '',
+        phoneNumber: '',
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const { name, value } = e.target;
+        setBillingAddress((prev: Billing) => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        // Dispatch the billing address to the CheckoutContext
+        dispatch({ type: 'SET_BILLING_ADDRESS', payload: billingAddress });
+    };
+
     return (
         <div>
             <h1 className="text-black bg-white">Billing Address</h1>
-            <form className='flex flex-col gap-6 p-6 shadow-md bg-white'>
+            <form
+                className="flex flex-col gap-6 p-6 shadow-md bg-white"
+                onSubmit={handleSubmit}
+            >
                 <div>
                     <label htmlFor="firstName" className="block mb-1 font-medium text-black">First Name:</label>
                     <input
                         type="text"
                         id="firstName"
                         name="firstName"
+                        value={billingAddress.firstName}
+                        onChange={handleChange}
                         className="w-full p-2 border border-black rounded text-black"
                     />
                 </div>
@@ -20,6 +52,8 @@ const BillingAddress: React.FC = () => {
                         type="text"
                         id="lastName"
                         name="lastName"
+                        value={billingAddress.lastName}
+                        onChange={handleChange}
                         className="w-full p-2 border border-black rounded text-black"
                     />
                 </div>
@@ -29,6 +63,8 @@ const BillingAddress: React.FC = () => {
                         type="text"
                         id="streetAddress"
                         name="streetAddress"
+                        value={billingAddress.streetAddress}
+                        onChange={handleChange}
                         className="w-full p-2 border border-black rounded text-black"
                     />
                 </div>
@@ -38,6 +74,8 @@ const BillingAddress: React.FC = () => {
                         type="text"
                         id="city"
                         name="city"
+                        value={billingAddress.city}
+                        onChange={handleChange}
                         className="w-full p-2 border border-black rounded text-black"
                     />
                 </div>
@@ -46,6 +84,8 @@ const BillingAddress: React.FC = () => {
                     <select
                         id="province"
                         name="province"
+                        value={billingAddress.province}
+                        onChange={handleChange}
                         className="w-full p-2 border border-black rounded text-black"
                     >
                         <option value="">Select Province/Territory</option>
@@ -62,7 +102,6 @@ const BillingAddress: React.FC = () => {
                         <option value="NT">Northwest Territories</option>
                         <option value="NU">Nunavut</option>
                         <option value="YT">Yukon</option>
-                        {/* Add more provinces as needed */}
                     </select>
                 </div>
                 <div>
@@ -71,6 +110,8 @@ const BillingAddress: React.FC = () => {
                         type="text"
                         id="postalCode"
                         name="postalCode"
+                        value={billingAddress.postalCode}
+                        onChange={handleChange}
                         className="w-full p-2 border border-black rounded text-black"
                     />
                 </div>
@@ -80,9 +121,17 @@ const BillingAddress: React.FC = () => {
                         type="text"
                         id="phoneNumber"
                         name="phoneNumber"
+                        value={billingAddress.phoneNumber}
+                        onChange={handleChange}
                         className="w-full p-2 border border-black rounded text-black"
                     />
                 </div>
+                <button
+                    type="submit"
+                    className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+                >
+                    Save Billing Address
+                </button>
             </form>
         </div>
     );
