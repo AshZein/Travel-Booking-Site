7c977495b036e655b364749646b504431bb8a06f
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2025-03-27 17:04:16 -0400

    create itinerary

--- src/app/api/checkout/submit/route.js
+++ src/app/api/checkout/submit/route.js
@@ -1,5 +1,16 @@
+import { prisma } from "@/utils/db";
+import { NextResponse } from "next/server";
+import { verifyToken } from "@/utils/auth";
+import { generateItineraryRef } from "@/utils/itinerary";
+
 export async function POST(request) {
     try {
+        // verify user token
+        const user = verifyToken(request);
+        if (!user) {
+            return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+        }
+
         // Parse the request body
         const {
             flightCredentials,
@@ -38,6 +49,77 @@ export async function POST(request) {
         //     selectedOutboundFlights,
         //     selectedReturnFlights,
         // });
+
+        // Create a new itinerary
+        const itineraryRef = await generateItineraryRef();
+        const itinerary = prisma.itinerary.create({
+            where: {
+                userId: user.userId,
+                itineraryRef: itineraryRef
+            },
+        });
+
+        // book outbound flight if selected
+        if (selectedOutboundFlights.length > 0) {
+            const outboundFlight = fetch("/api/flight/booking",
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        firstName: flightCredentials.firstName,
+                        lastName: flightCredentials.lastName,
+                        email: flightCredentials.email,
+                        passportNumber: flightCredentials.passportNumber,
+                        flightIds: selectedOutboundFlights,
+                    }),
+                }
+            );
+            prisma.itinerary.update({
+                where: {
+                    userId: user.userId,
+                    itineraryRef: itineraryRef
+                },
+                data: {
+                    outboundFlightBookingRef: outboundFlight.bookingReference,
+                }
+            });
+        }
+
+        // book return flight if selected
+        if (selectedReturnFlights.length > 0) {
+            const returnFlight = fetch("/api/flight/booking",
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        firstName: flightCredentials.firstName,
+                        lastName: flightCredentials.lastName,
+                        email: flightCredentials.email,
+                        passportNumber: flightCredentials.passportNumber,
+                        flightIds: selectedOutboundFlights,
+                    }),
+                }
+            );
+
+            prisma.itinerary.update({
+                where: {
+                    userId: user.userId,
+                    itineraryRef: itineraryRef
+                },
+                data: {
+                    outboundFlightBookingRef: returnFlight.bookingReference,
+                }
+            });
+        }
+
+
+        // book hotel if selected
+
+
 
         // Return a success response
         return new Response(
