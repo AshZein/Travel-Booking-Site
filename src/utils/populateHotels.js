a90984dfc748203e42e3b0a8b1467748fab6a152
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2025-04-02 20:22:33 -0400

    add paths to prepopulated hotel images

--- src/utils/populateHotels.js
+++ src/utils/populateHotels.js
@@ -5,7 +5,29 @@ const hotels1000Path = "../../importData/hotels_1000.json";
 import fs from 'fs';
 
 const hotelsData = JSON.parse(fs.readFileSync(new URL(hotels1000Path, import.meta.url), 'utf-8'));
-const baseImgPath = "src/images";
+const roomImgPaths = {
+    double: [
+        "src/images/hotel/room/double/Altair-QQ-1-LG-1920x1281.jpg",
+        "src/images/hotel/room/double/e8c50ce285b0a5730a23db4e1459cd3b.jpg",
+        "src/images/hotel/room/double/empire-hotel-nyc-superior-room-two-double-beds-01-high-res-2020_wide.jpg",
+        "src/images/hotel/room/double/images (1).jpeg",
+        "src/images/hotel/room/double/images.jpeg"
+    ],
+    single: [
+        "src/images/hotel/room/single/download.jpeg",
+        "src/images/hotel/room/single/images.jpeg",
+        "src/images/hotel/room/single/king-blue-hotel-toronto-royal-suite-1-bed-02_wide.jpg",
+        "src/images/hotel/room/single/Single-bed-room-YWCA_Hotel_Vancouver.jpg"
+    ],
+    suite: [
+        "src/images/hotel/room/suite/images (1).jpeg",
+        "src/images/hotel/room/suite/images (2).jpeg",
+        "src/images/hotel/room/suite/images (3).jpeg",
+        "src/images/hotel/room/suite/images.jpeg",
+        "src/images/hotel/room/suite/istockphoto-1452529483-612x612.jpg"
+    ]
+};
+
 export async function fillHotel() {
     // Iterate over the keys of the hotelsData object (e.g., hotel1, hotel2, ...)
     for (let hotelKey of Object.keys(hotelsData)) {
@@ -49,10 +71,10 @@ export async function fillHotel() {
 
             
             // add room images
-            for (let image of room.images) {
+            for (let image of roomImgPaths[room.roomType]) {
                 const imageData = {
                     hotelRoomTypeId: createdRoom.hotelRoomTypeId,
-                    image: baseImgPath + "/hotel/room/" + image,
+                    image: image,
                 };
                 await prisma.roomImage.create({
                     data: imageData,
