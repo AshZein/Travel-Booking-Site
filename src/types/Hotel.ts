43aa5e4743a35ddd8008ac9cf2efb7aadbc6bcc4
Ashkan Zeinolabedini <ashkan-oakville@outlook.com>
2025-04-03 12:09:15 -0400

    full hotel info

--- src/types/Hotel.ts
+++ src/types/Hotel.ts
@@ -2,6 +2,11 @@ export interface Hotel {
     hotelId: number;
     name: string;
     address: string;
+    city: string;
+    country: string;
+    latitude: number;
+    longitude: number;
     starRating: number;
+    amenities: string[];
     startingPrice: number;
   }
