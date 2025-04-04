0817a057e35b806b03966bf40eca9642a846d296
Youssef A <Youssef.abouzied@mail.utoronto.ca>
2025-04-04 19:24:36 -0400

    psuh

--- src/types/hotel.ts
+++ src/types/hotel.ts
@@ -1,12 +1,12 @@
 export interface Hotel {
-  hotelId: number;
-  name: string;
-  logo: string;
-  address: string;
-  city: string;
-  country: string;
-  latitude: number;
-  longitude: number;
-  starRating: number;
-  
-}
+    hotelId: number;
+    name: string;
+    address: string;
+    city: string;
+    country: string;
+    latitude: number;
+    longitude: number;
+    starRating: number;
+    amenities: string[];
+    startingPrice: number;
+  }
